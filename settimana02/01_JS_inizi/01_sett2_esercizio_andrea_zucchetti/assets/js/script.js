09c64db853bccffdbcf00c474472ac5dab41ff78
Zoosedesign <andre.zucchetti@me.com>
2023-04-03 16:33:45 +0200

    prova

--- settimana02/01_JS_inizi/01_sett2_esercizio_andrea_zucchetti/assets/js/script.js
+++ settimana02/01_JS_inizi/01_sett2_esercizio_andrea_zucchetti/assets/js/script.js
@@ -1,11 +1,13 @@
 alert('questa è la pagina 2')
 
-document.getElementById('outputPrimo').innerHTML = 'output scritto da javascript'
+document.getElementById('outputPrimo').innerHTML = 'output<span class="PROGRAMMA"></span>'
 
-document.getElementById('PANTERA').innerHTML = 'pantera scritto da javascript'
+document.getElementById('PANTERA').innerHTML = 'pantera<span class="PROGRAMMA"></span>'
 
 console.log('criceto')
 console.log('giraffa')
 console.log('leone')
 
-document.write('<h3>Esercizio di allenamento a Javascript</h3>')
+document.write('<h3>Esercizio di allenamento a Javascript</h3>');
+
+setTimeout(document.write, 2000);
