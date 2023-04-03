9fcaa4e68317996707f7ccc4bc83e6d528173eae
Zoosedesign <andre.zucchetti@me.com>
2023-04-03 16:45:39 +0200

    def

--- settimana02/01_JS_inizi/01_sett2_esercizio_andrea_zucchetti/assets/js/script.js
+++ settimana02/01_JS_inizi/01_sett2_esercizio_andrea_zucchetti/assets/js/script.js
@@ -1,13 +1,11 @@
 alert('questa è la pagina 2')
 
-document.getElementById('outputPrimo').innerHTML = 'output<span class="PROGRAMMA"></span>'
+document.getElementById('outputPrimo').innerHTML = 'output<span id="PROGRAMMA"></span>'
 
-document.getElementById('PANTERA').innerHTML = 'pantera<span class="PROGRAMMA"></span>'
+document.getElementById('PANTERA').innerHTML = 'pantera è scritta in javascript'
 
 console.log('criceto')
 console.log('giraffa')
 console.log('leone')
 
 document.write('<h3>Esercizio di allenamento a Javascript</h3>');
-
-setTimeout(document.write, 2000);
--- settimana02/01_JS_inizi/01_sett2_esercizio_andrea_zucchetti/assets/js/scriptVariables.js
+++ settimana02/01_JS_inizi/01_sett2_esercizio_andrea_zucchetti/assets/js/scriptVariables.js
@@ -1,7 +1,7 @@
-var programma = 'è scritto da Javascript';
+var programma = ' è scritto da Javascript';
 
 if (programma === undefined) {
-    document.getElementsByClassName('PROGRAMMA').innerHTML = 'non è scritto in javascript';
+    document.getElementById('PROGRAMMA').innerHTML = 'non è scritto in javascript';
 } else {
-    document.getElementsByClassName('PROGRAMMA').innerHTML = programma
+    document.getElementById('PROGRAMMA').innerHTML = programma
 }
