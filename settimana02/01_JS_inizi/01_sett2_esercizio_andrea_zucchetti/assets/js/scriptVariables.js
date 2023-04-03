b0b9ea31d967e070fb71e748546e97cfb8656f2d
Zoosedesign <andre.zucchetti@me.com>
2023-04-03 16:46:44 +0200

    def

--- settimana02/01_JS_inizi/01_sett2_esercizio_andrea_zucchetti/assets/js/scriptVariables.js
+++ settimana02/01_JS_inizi/01_sett2_esercizio_andrea_zucchetti/assets/js/scriptVariables.js
@@ -1,7 +1,7 @@
 var programma = ' è scritto da Javascript';
 
 if (programma === undefined) {
-    document.getElementById('PROGRAMMA').innerHTML = 'non è scritto in javascript';
+    document.getElementById('PROGRAMMA').innerHTML = ' non è scritto in javascript';
 } else {
     document.getElementById('PROGRAMMA').innerHTML = programma
 }
