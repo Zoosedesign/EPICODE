315e0b5dc3e9b90a7c63195c31a4ed7b2873a564
Zoosedesign <andre.zucchetti@me.com>
2023-05-03 18:23:13 +0200

    def

--- settimana06/02_bootstrap/assets/js/script.js
+++ settimana06/02_bootstrap/assets/js/script.js
@@ -10,26 +10,42 @@ window.addEventListener('scroll', function () {
 });
 
 //popolamento card section saldi
-const prodotti = [
+const product = [
   {
     title: 'Card title',
     text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem quia corporis amet, non est dolores?',
     img: 'assets/img/img03.jpg',
+    img2: 'assets/img/img01.jpg',
   },
   {
     title: 'Card title',
     text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem quia corporis amet, non est dolores?',
     img: 'assets/img/img03.jpg',
+    img2: 'assets/img/img01.jpg',
   },
   {
     title: 'Card title',
     text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit?',
     img: 'assets/img/img03.jpg',
+    img2: 'assets/img/img01.jpg',
   },
   {
     title: 'Card title',
     text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem quia corporis amet, non est dolores?',
     img: 'assets/img/img03.jpg',
+    img2: 'assets/img/img01.jpg',
+  },
+  {
+    title: 'Card title',
+    text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem quia corporis amet, non est dolores?',
+    img: 'assets/img/img03.jpg',
+    img2: 'assets/img/img01.jpg',
+  },
+  {
+    title: 'Card title',
+    text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem quia corporis amet, non est dolores?',
+    img: 'assets/img/img03.jpg',
+    img2: 'assets/img/img01.jpg',
   },
 ];
 
@@ -38,13 +54,28 @@ const prodotti = [
 // inserisco dentro l'elemento
 let row = document.getElementById('saldi-product');
 
-for (let i = 0; i < prodotti.length; i++) {
+for (let i = 0; i < 4; i++) {
   row.innerHTML += `<div class="col-6 col-lg-4 col-xl-3 px-3 mb-5 mb-lg-0">
  <div class="card rounded-5 shadow">
-  <img src="${prodotti[i].img}" class="card-img-top" alt="residence maldive">
+  <img src="${product[i].img}" class="card-img-top" alt="residence maldive">
   <article class="card-body d-flex flex-column bg-white rounded-5 rounded-top-0">
-    <h4 class="card-title text-secondary">${prodotti[i].title}</h4>
-    <p class="card-text">${prodotti[i].text}</p>
+    <h4 class="card-title text-secondary">${product[i].title}</h4>
+    <p class="card-text">${product[i].text}</p>
+    <a href="#" class="btn btn-primary rounded-pill mt-auto px-4 align-self-end"><i class="fa-solid fa-suitcase-rolling"></i> Prenota</a>
+  </article>
+ </div>
+</div>`
+}
+
+let row2 = document.getElementById('summer-product');
+
+for (let i = 0; i < product.length; i++) {
+  row2.innerHTML += `<div class="col-6 col-xl-4 px-3 mb-5">
+ <div class="card rounded-5 shadow">
+  <img src="${product[i].img2}" class="card-img-top" alt="residence maldive">
+  <article class="card-body d-flex flex-column bg-white rounded-5 rounded-top-0">
+    <h4 class="card-title text-secondary">${product[i].title}</h4>
+    <p class="card-text">${product[i].text}</p>
     <a href="#" class="btn btn-primary rounded-pill mt-auto px-4 align-self-end"><i class="fa-solid fa-suitcase-rolling"></i> Prenota</a>
   </article>
  </div>
