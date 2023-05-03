3f512408f324c23b632f7749869b35a2f9822514
Zoosedesign <andre.zucchetti@me.com>
2023-05-03 17:33:24 +0200

    def

--- settimana06/02_bootstrap/assets/js/script.js
+++ settimana06/02_bootstrap/assets/js/script.js
@@ -1,108 +1,52 @@
+//effetto scroll navbar
+var header = document.querySelector('header');
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY > 32) {
+    header.classList.add('rounded-top-0');
+  } else {
+    header.classList.remove('rounded-top-0');
+  }
+});
+
+//popolamento card section saldi
 const prodotti = [
   {
-    id: 1,
-    title: "iPhone 9",
-    description: "An apple mobile which is nothing like apple",
-    price: 549,
-    discountPercentage: 12.96,
-    rating: 4.69,
-    stock: 94,
-    brand: "Apple",
-    category: "smartphones",
-    thumbnail: "https://i.dummyjson.com/data/products/1/thumbnail.jpg",
-    images: [
-      "https://i.dummyjson.com/data/products/1/1.jpg",
-      "https://i.dummyjson.com/data/products/1/2.jpg",
-      "https://i.dummyjson.com/data/products/1/3.jpg",
-      "https://i.dummyjson.com/data/products/1/4.jpg",
-      "https://i.dummyjson.com/data/products/1/thumbnail.jpg",
-    ],
+    title: 'Card title',
+    text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem quia corporis amet, non est dolores?',
+    img: 'assets/img/img03.jpg',
   },
   {
-    id: 2,
-    title: "iPhone X",
-    description:
-      "SIM-Free, Model A19211 6.5-inch Super Retina HD display with OLED technology A12 Bionic chip with ...",
-    price: 899,
-    discountPercentage: 17.94,
-    rating: 4.44,
-    stock: 34,
-    brand: "Apple",
-    category: "smartphones",
-    thumbnail: "https://i.dummyjson.com/data/products/2/thumbnail.jpg",
-    images: [
-      "https://i.dummyjson.com/data/products/2/1.jpg",
-      "https://i.dummyjson.com/data/products/2/2.jpg",
-      "https://i.dummyjson.com/data/products/2/3.jpg",
-      "https://i.dummyjson.com/data/products/2/thumbnail.jpg",
-    ],
+    title: 'Card title',
+    text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem quia corporis amet, non est dolores?',
+    img: 'assets/img/img03.jpg',
   },
   {
-    id: 3,
-    title: "Samsung Universe 9",
-    description:
-      "Samsung's new variant which goes beyond Galaxy to the Universe",
-    price: 1249,
-    discountPercentage: 15.46,
-    rating: 4.09,
-    stock: 36,
-    brand: "Samsung",
-    category: "smartphones",
-    thumbnail: "https://i.dummyjson.com/data/products/3/thumbnail.jpg",
-    images: ["https://i.dummyjson.com/data/products/3/1.jpg"],
+    title: 'Card title',
+    text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit?',
+    img: 'assets/img/img03.jpg',
   },
   {
-    id: 4,
-    title: "OPPOF19",
-    description: "OPPO F19 is officially announced on April 2021.",
-    price: 280,
-    discountPercentage: 17.91,
-    rating: 4.3,
-    stock: 123,
-    brand: "OPPO",
-    category: "smartphones",
-    thumbnail: "https://i.dummyjson.com/data/products/4/thumbnail.jpg",
-    images: [
-      "https://i.dummyjson.com/data/products/4/1.jpg",
-      "https://i.dummyjson.com/data/products/4/2.jpg",
-      "https://i.dummyjson.com/data/products/4/3.jpg",
-      "https://i.dummyjson.com/data/products/4/4.jpg",
-      "https://i.dummyjson.com/data/products/4/thumbnail.jpg",
-    ],
+    title: 'Card title',
+    text: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem quia corporis amet, non est dolores?',
+    img: 'assets/img/img03.jpg',
   },
-  {
-    id: 5,
-    title: "Huawei P30",
-    description:
-      "Huawei’s re-badged P30 Pro New Edition was officially unveiled yesterday in Germany and now the device has made its way to the UK.",
-    price: 499,
-    discountPercentage: 10.58,
-    rating: 4.09,
-    stock: 32,
-    brand: "Huawei",
-    category: "smartphones",
-    thumbnail: "https://i.dummyjson.com/data/products/5/thumbnail.jpg",
-    images: [
-      "https://i.dummyjson.com/data/products/5/1.jpg",
-      "https://i.dummyjson.com/data/products/5/2.jpg",
-      "https://i.dummyjson.com/data/products/5/3.jpg",
-    ],
-  },
-]
+];
 
 // prendo l'elemento da riempire
 // creo la card di bootstrap
 // inserisco dentro l'elemento
-let row = document.querySelector(".row#prodotti")
+let row = document.getElementById('saldi-product');
 
 for (let i = 0; i < prodotti.length; i++) {
-  row.innerHTML += `<div class="col col-2">  <div class="card">
-    <div class="card-body">
-      <h5 class="card-title">${prodotti[i].title}</h5>
-      <img class="w-100" src="${prodotti[i].thumbnail}" />
-      <button onclick="nomeFunzione(event)" class="btn btn-success"> Carrello </button>
-    </div> 
-  </div> </div>`
+  row.innerHTML += `<div class="col-6 col-lg-4 col-xl-3 px-3 mb-5 mb-lg-0">
+ <div class="card rounded-5 shadow">
+  <img src="${prodotti[i].img}" class="card-img-top" alt="residence maldive">
+  <article class="card-body d-flex flex-column bg-white rounded-5 rounded-top-0">
+    <h4 class="card-title text-secondary">${prodotti[i].title}</h4>
+    <p class="card-text">${prodotti[i].text}</p>
+    <a href="#" class="btn btn-primary rounded-pill mt-auto px-4 align-self-end"><i class="fa-solid fa-suitcase-rolling"></i> Prenota</a>
+  </article>
+ </div>
+</div>`
 }
-
-
