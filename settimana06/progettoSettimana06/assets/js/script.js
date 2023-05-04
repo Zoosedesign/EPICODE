d64affce81b3d9d5eb278cb7b8665ac7539430e4
Zoosedesign <andre.zucchetti@me.com>
2023-05-05 01:19:05 +0200

    def

---
+++ settimana06/progettoSettimana06/assets/js/script.js
@@ -0,0 +1,81 @@
+//popolamento dropdown NAV
+const nav = [
+    {
+        title: 'Brownse',
+        link1: {
+            url: '#',
+            item: 'Primo link'
+        },
+        link2: {
+            url: '#',
+            item: 'Secondo link'
+        },
+        link3: {
+            url: '#',
+            item: 'Terzo link'
+        },
+    },
+    {
+        title: 'Landings',
+        link1: {
+            url: '#',
+            item: 'Primo link'
+        },
+        link2: {
+            url: '#',
+            item: 'Secondo link'
+        },
+        link3: {
+            url: '#',
+            item: 'Terzo link'
+        },
+    },
+    {
+        title: 'Pages',
+        link1: {
+            url: '#',
+            item: 'Primo link'
+        },
+        link2: {
+            url: '#',
+            item: 'Secondo link'
+        },
+        link3: {
+            url: '#',
+            item: 'Terzo link'
+        },
+    },
+    {
+        title: 'Accounts',
+        link1: {
+            url: '#',
+            item: 'Primo link'
+        },
+        link2: {
+            url: '#',
+            item: 'Secondo link'
+        },
+        link3: {
+            url: '#',
+            item: 'Terzo link'
+        },
+    },
+];
+
+// dropdown NAV
+const ul = document.querySelector('ul');
+
+for (let i = 0; i < nav.length; i++) {
+    ul.innerHTML += `<li class="nav-item dropdown ms-2">
+    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+        aria-expanded="false">${nav[i].title}</a>
+    <ul class="dropdown-menu">
+        <li><a class="dropdown-item" href="${nav[i].link1.url}">${nav[i].link1.item}</a></li>
+        <li><a class="dropdown-item" href="${nav[i].link2.url}">${nav[i].link2.item}</a></li>
+        <li>
+            <hr class="dropdown-divider">
+        </li>
+        <li><a class="dropdown-item" href="${nav[i].link3.url}">${nav[i].link3.item}</a></li>
+    </ul>
+</li>`
+}
