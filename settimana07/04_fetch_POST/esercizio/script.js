1655c9fd49115b862e1ebbe72fc777798f4107f2
Zoosedesign <andre.zucchetti@me.com>
2023-05-11 17:42:40 +0200

    def

--- settimana07/04_fetch_POST/esercizio/script.js
+++ settimana07/04_fetch_POST/esercizio/script.js
@@ -1,9 +1,27 @@
+//-------------------- VARIABILI GLOBALI ------------------------
+const searchForm = document.getElementById('search-form');
+const searchInput = document.getElementById('search-input');
 const apiKey = 'x8g58VNX0dYe9bETS6WALS6uk82AxyfPAFuKOtv7gJGPKfKkOhFIHGJB';
 
+
+//-------------------- SEARCH FORM ------------------------
+searchForm.addEventListener('submit', (event) => {
+      event.preventDefault();
+      // il termine di ricerca (es:"cane") viene preso e tramite il metodo "trim()" vengono rimossi eventuali spazi vuoti all'inizio e alla fine in modo da evitare errori
+      const formQuery = searchInput.value.trim();
+      
+      if (formQuery) {
+        // se la stringa di ricerca non è vuota effettuiamo la ricerca nell'API
+        searchImages(formQuery);
+      }
+    });
+
+//-------------------- CREAZIONE CARD ------------------------
 //il bottone passerà la "query" cioè la parola di ricerca
 function searchImages(query) {
   //svuoto il contenuto  di "queryCardBox" ogni volta che clicco uno dei due bottoni
   document.getElementById("queryCardBox").innerHTML = "";
+  searchInput.value = "";
 
   fetch(`https://api.pexels.com/v1/search?query=${query}`, {
     headers: {
@@ -39,7 +57,7 @@ function searchImages(query) {
                       Hide
                     </button>
                   </div>
-                  <small class="text-muted">9 mins</small>
+                  <small class="text-muted">${duck.id}</small>
                 </div>
               </div>
             </div>
