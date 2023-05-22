fb7e4d62096bffc1c9d331cde015f8aa98c29727
Zoosedesign <andrea.zucchetti@me.com>
2023-05-22 17:02:29 +0200

    def

---
+++ settimana09/01_typescript/assets/js/classi.js
@@ -0,0 +1,12 @@
+"use strict";
+class Studente {
+    constructor(_nome, _cognome) {
+        this.nome = _nome;
+        this.cognome = _cognome;
+    }
+    getStudente() {
+        return `${this.nome} ${this.cognome}`;
+    }
+}
+let newStudente = new Studente('Mario', 'Rossi');
+console.log(newStudente);
---
+++ settimana09/01_typescript/assets/ts/esercizio_01_set04.ts
@@ -0,0 +1,19 @@
+let randomNumber = (): number => {
+    return Math.floor(Math.random() * 100) + 1;
+};
+
+let numberExtract: number = randomNumber();
+const player1: number = 45;
+const player2: number = 35;
+
+if (Math.abs(numberExtract - player1) < Math.abs(numberExtract - player2)) {
+    console.log(`Il giocatore 1 si è avvicinato di più a ${numberExtract}`);
+} else if (numberExtract === player1) {
+    console.log(`Il giocatore 1 ha azzeccato il risultato, il numero era ${numberExtract}`);
+} else if (Math.abs(numberExtract - player2) < Math.abs(numberExtract - player1)) {
+    console.log(`Il giocatore 2 si è avvicinato di più a ${numberExtract}`);
+} else if (numberExtract === player2) {
+    console.log(`Il giocatore 2 ha azzeccato il risultato, il numero era ${numberExtract}`);
+} else {
+    console.log("Entrambi i giocatori si sono avvicinati in egual misura al risultato");
+}
