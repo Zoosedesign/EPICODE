5ed3604bfa8d66f2f5bcab5a2b17318e977e4040
Zoosedesign <andrea.zucchetti@me.com>
2023-06-09 09:57:18 +0200

    def

--- ANGULAR/movies_project/src/environments/environment.ts
+++ ANGULAR/movies_project/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   // in modo da non dover scriverlo in tutte le chiamate
-  baseURL: 'http://localhost:4201/'
+  baseURL: 'http://localhost:4201/api'
 };
