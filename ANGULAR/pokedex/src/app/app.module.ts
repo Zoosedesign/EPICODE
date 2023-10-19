103d8a481ddc029c2e3028f9fc6c4d0da84edffd
Zoosedesign <andrea.zucchetti@me.com>
2023-10-19 11:47:29 +0200

    def

---
+++ ANGULAR/pokedex/src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { PokedexComponent } from './pages/pokedex/pokedex.component';
+import { CardComponent } from './components/card/card.component';
+
+const pages:Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'pokedex',
+    component: PokedexComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: '**', //tutte le altre rotte non definite
+    component: HomeComponent,
+    redirectTo: ''
+  }
+]
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    PokedexComponent,
+    CardComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(pages)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
