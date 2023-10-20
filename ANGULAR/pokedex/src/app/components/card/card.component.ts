0c8f6eb3228faacc84435e9ad77c05c6212bb206
Zoosedesign <andrea.zucchetti@me.com>
2023-10-20 11:05:54 +0200

    def

--- ANGULAR/pokedex/src/app/components/card/card.component.ts
+++ ANGULAR/pokedex/src/app/components/card/card.component.ts
@@ -15,14 +15,14 @@ export class CardComponent implements OnInit {
   constructor(private PokemonSrv: PokemonService) { }
 
   ngOnInit(): void {
-    this.fetchData()
+    this.getPokemon()
   }
 
-  fetchData(): void {
-    this.PokemonSrv.get<Pagination>('https://pokeapi.co/api/v2/pokemon?limit=151').subscribe(data => {
+  getPokemon(): void {
+    this.PokemonSrv.loadContent<Pagination>('https://pokeapi.co/api/v2/pokemon?limit=151').subscribe(data => {
       const pokemonsUrl: Pokemons[] = data.results;
       pokemonsUrl.forEach(pokemonUrl => {
-        this.PokemonSrv.get<Pokemon>(pokemonUrl.url).subscribe(pokemon => {
+        this.PokemonSrv.loadContent<Pokemon>(pokemonUrl.url).subscribe(pokemon => {
           this.pokemons.push(pokemon);
         })
       });
--- ANGULAR/pokedex/src/app/services/pokemon.service.ts
+++ ANGULAR/pokedex/src/app/services/pokemon.service.ts
@@ -2,15 +2,20 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+import { Pagination } from '../models/pagination.interface';
+import { Pokemons } from '../models/pokemons.interface';
+import { Pokemon } from '../models/pokemon.interface';
+
 @Injectable({
   providedIn: 'root'
 })
 export class PokemonService {
+  pokemons: Pokemon[] = []
 
   constructor(private http: HttpClient) { }
 
   //metto <type> per utilizzare il metodo con diversi oggetti
-  get<type>(url: string): Observable<type> {
+  loadContent<type>(url: string): Observable<type> {
     return this.http.get<type>(`${url}`)
   }
 }
