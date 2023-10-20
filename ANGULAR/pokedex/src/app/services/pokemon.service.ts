fc4c9abb7f9be56bdfebf1ea79e941e59f53e8e3
Zoosedesign <andrea.zucchetti@me.com>
2023-10-20 15:48:28 +0200

    def

--- ANGULAR/pokedex/src/app/services/pokemon.service.ts
+++ ANGULAR/pokedex/src/app/services/pokemon.service.ts
@@ -2,15 +2,10 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-import { Pagination } from '../models/pagination.interface';
-import { Pokemons } from '../models/pokemons.interface';
-import { Pokemon } from '../models/pokemon.interface';
-
 @Injectable({
   providedIn: 'root'
 })
 export class PokemonService {
-  pokemons: Pokemon[] = []
 
   constructor(private http: HttpClient) { }
 
